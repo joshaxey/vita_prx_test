fdb7e6e4e56676b9186a74a693fcc1a3f7c72488
Josh_Axey <josh.axey.3006@gmail.com>
2016-08-19 17:17:54 +1000

    Mucking about while on the road
    This is broken junk, just writing from the browser while out and about

--- prx/main.c
+++ prx/main.c
@@ -1,4 +1,30 @@
+#include <stdio.h>
+
+#include <psp2/ctrl.h>
+#include <psp2/kernel/modulemgr.h>
+#include <psp2/moduleinfo.h>
+#include <psp2/power.h>
+
+#define freq = 444
+
+extern "C" {
+	int scePowerSetArmClockFrequency(int freq);
+}
+
+int module_start(SceSize args, void *argp) {
+  SceUID thid;
+
+  scePowerSetArmClockFrequency(freq);
+
+  return 0;
+}
+
+int module_stop(void) {
+  return 0;
+}
 
 int main() {
-  	return 0xDEADBEEF;
+  module_start();
+  module_stop();
+  return 0xDEADBEEF;
 }
